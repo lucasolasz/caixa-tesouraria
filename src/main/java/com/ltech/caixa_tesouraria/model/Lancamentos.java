dcd11637da1369cd1056ae6c0f85a5617504411a
conuaz <lucas.olasz@tty2000.com.br>
2025-07-17 14:36:06 -0300

    Classes modelo

---
+++ src/main/java/com/ltech/caixa_tesouraria/model/Lancamentos.java
@@ -0,0 +1,43 @@
+package com.ltech.caixa_tesouraria.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lancamentos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataLancamento;
+
+    @Column(length = 255)
+    private String descricao;
+
+    @ManyToOne
+    private Usuario funcionarioLancamento;
+
+    @ManyToOne
+    private TipoMovimentacao tipoMovimentacao;
+
+    @ManyToOne
+    private FundoFinanceiro fundoFinanceiro;
+
+    @Column(precision = 19, scale = 2)
+    private BigDecimal valor;
+
+}
