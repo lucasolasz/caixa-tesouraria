f13dfcd44a3a52f2755e65eb90f9dff2830d9060
conuaz <lucas.olasz@tty2000.com.br>
2025-08-08 17:59:24 -0300

    Remove entidades não utilizadas

--- src/main/java/com/ltech/caixa_tesouraria/repository/FormaPagamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ltech.caixa_tesouraria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ltech.caixa_tesouraria.model.FormaPagamento;
-
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
-
-}
