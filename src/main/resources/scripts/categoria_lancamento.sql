c2d2e9cd92d27f51dab476d4eac53cb32366b12f
conuaz <lucas.olasz@tty2000.com.br>
2025-08-12 18:00:16 -0300

    Inclui categoria para o lançamento

---
+++ src/main/resources/scripts/categoria_lancamento.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    tesouraria.categoria_lancamento (id, descricao)
+VALUES
+    (1, 'Ofertas de Culto'),
+    (2, 'Dízimos'),
+    (3, 'Ofertas Especiais'),
+    (4, 'Compras de Materiais'),
+    (5, 'Manutenção e Infraestrutura'),
+    (6, 'Eventos Comunitários'),
+    (7, 'Outras Despesas Administrativas');
