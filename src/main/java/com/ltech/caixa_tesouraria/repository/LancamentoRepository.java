917d7b18aa4857dcfaf1f40e7a633b72077cad69
lucasolasz <olaszlucas072@gmail.com>
2025-08-13 21:31:28 -0300

    Ajusta somatório da home

--- src/main/java/com/ltech/caixa_tesouraria/repository/LancamentoRepository.java
+++ src/main/java/com/ltech/caixa_tesouraria/repository/LancamentoRepository.java
@@ -17,49 +17,55 @@ import com.ltech.caixa_tesouraria.model.TipoMovimentacao;
 
 public interface LancamentoRepository extends JpaRepository<Lancamentos, Long> {
 
-    @Query("""
-                SELECT l FROM Lancamentos l
-                WHERE LOWER(l.descricao) LIKE LOWER(CONCAT('%', :search, '%'))
-                   OR CAST(l.dataLancamento AS string) LIKE CONCAT('%', :search, '%')
-                   OR CAST(l.valor AS string) LIKE CONCAT('%', :search, '%')
-                   OR LOWER(l.tipoMovimentacao.descricao) LIKE LOWER(CONCAT('%', :search, '%'))
-                   OR LOWER(l.fundoFinanceiro.descricao) LIKE LOWER(CONCAT('%', :search, '%'))
-                   OR LOWER(l.funcionarioLancamento.username) LIKE LOWER(CONCAT('%', :search, '%'))
-            """)
-    Page<Lancamentos> searchAllColumns(@Param("search") String search, Pageable pageable);
+        @Query("""
+                            SELECT l FROM Lancamentos l
+                            WHERE LOWER(l.descricao) LIKE LOWER(CONCAT('%', :search, '%'))
+                               OR CAST(l.dataLancamento AS string) LIKE CONCAT('%', :search, '%')
+                               OR CAST(l.valor AS string) LIKE CONCAT('%', :search, '%')
+                               OR LOWER(l.tipoMovimentacao.descricao) LIKE LOWER(CONCAT('%', :search, '%'))
+                               OR LOWER(l.fundoFinanceiro.descricao) LIKE LOWER(CONCAT('%', :search, '%'))
+                               OR LOWER(l.funcionarioLancamento.username) LIKE LOWER(CONCAT('%', :search, '%'))
+                        """)
+        Page<Lancamentos> searchAllColumns(@Param("search") String search, Pageable pageable);
 
-    @Query("""
-                SELECT SUM(
-                    CASE
-                        WHEN l.tipoMovimentacao.id = 1 THEN l.valor
-                        WHEN l.tipoMovimentacao.id = 2 THEN -l.valor
-                        ELSE 0
-                    END
-                )
-                FROM Lancamentos l
-                WHERE MONTH(l.dataLancamento) = :numeroDoMes
-                  AND YEAR(l.dataLancamento) = :numeroAno
-            """)
-    BigDecimal getValorTotalGanhosMesCorrente(@Param("numeroDoMes") int numeroDoMes,
-            @Param("numeroAno") int numeroAno);
+        @Query("""
+                            SELECT SUM(
+                                CASE
+                                    WHEN l.tipoMovimentacao.id = 1 THEN l.valor
+                                    WHEN l.tipoMovimentacao.id = 2 THEN -l.valor
+                                    ELSE 0
+                                END
+                            )
+                            FROM Lancamentos l
+                            WHERE MONTH(l.dataLancamento) = :numeroDoMes
+                              AND YEAR(l.dataLancamento) = :numeroAno
+                        """)
+        BigDecimal getValorTotalGanhosMesCorrente(@Param("numeroDoMes") int numeroDoMes,
+                        @Param("numeroAno") int numeroAno);
 
-    @Query("""
-                SELECT SUM(valor) FROM Lancamentos l
-                WHERE YEAR(l.dataLancamento) = :numeroAno
-            """)
-    BigDecimal getValorTotalGanhosAno(@Param("numeroAno") int numeroAno);
+        @Query("""
+                            SELECT SUM(
+                            CASE
+                                    WHEN l.tipoMovimentacao.id = 1 THEN l.valor
+                                    WHEN l.tipoMovimentacao.id = 2 THEN -l.valor
+                                    ELSE 0
+                                END
+                                ) FROM Lancamentos l
+                            WHERE YEAR(l.dataLancamento) = :numeroAno
+                        """)
+        BigDecimal getValorTotalGanhosAno(@Param("numeroAno") int numeroAno);
 
-    @Query("SELECT l FROM Lancamentos l " +
-            "WHERE (:tipoMovimentacao IS NULL OR l.tipoMovimentacao = :tipoMovimentacao) " +
-            "AND (:categoriaLancamento IS NULL OR l.categoriaLancamento = :categoriaLancamento) " +
-            "AND (:fundoFinanceiro IS NULL OR l.fundoFinanceiro = :fundoFinanceiro) " +
-            "AND (:dataInicio IS NULL OR l.dataLancamento >= :dataInicio) " +
-            "AND (:dataFim IS NULL OR l.dataLancamento <= :dataFim) " +
-            "AND (:dataLancamento IS NULL OR l.dataLancamento = :dataLancamento)")
-    List<Lancamentos> getPesquisarComFiltro(@Param("tipoMovimentacao") TipoMovimentacao tipoMovimentacao,
-            @Param("categoriaLancamento") CategoriaLancamento categoriaLancamento,
-            @Param("fundoFinanceiro") FundoFinanceiro fundoFinanceiro,
-            @Param("dataInicio") LocalDate dataInicio,
-            @Param("dataFim") LocalDate dataFim,
-            @Param("dataLancamento") LocalDate dataLancamento);
+        @Query("SELECT l FROM Lancamentos l " +
+                        "WHERE (:tipoMovimentacao IS NULL OR l.tipoMovimentacao = :tipoMovimentacao) " +
+                        "AND (:categoriaLancamento IS NULL OR l.categoriaLancamento = :categoriaLancamento) " +
+                        "AND (:fundoFinanceiro IS NULL OR l.fundoFinanceiro = :fundoFinanceiro) " +
+                        "AND (:dataInicio IS NULL OR l.dataLancamento >= :dataInicio) " +
+                        "AND (:dataFim IS NULL OR l.dataLancamento <= :dataFim) " +
+                        "AND (:dataLancamento IS NULL OR l.dataLancamento = :dataLancamento)")
+        List<Lancamentos> getPesquisarComFiltro(@Param("tipoMovimentacao") TipoMovimentacao tipoMovimentacao,
+                        @Param("categoriaLancamento") CategoriaLancamento categoriaLancamento,
+                        @Param("fundoFinanceiro") FundoFinanceiro fundoFinanceiro,
+                        @Param("dataInicio") LocalDate dataInicio,
+                        @Param("dataFim") LocalDate dataFim,
+                        @Param("dataLancamento") LocalDate dataLancamento);
 }
